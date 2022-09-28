4b9e2fb4806797d5effeb90d972066648464795a
Mun-Seok <anstjr97@gmail.com>
2022-09-28 17:43:13 +0900

    mobile sidebar finished

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-let menuIcon =document.getElementById('menuIcon')
-let menu =document.getElementById('menu')
+let menuIcon = document.getElementById('menuIcon');
+let menuList = document.getElementById('menuList');
 
-menuIcon.addEventListener('click',()=>{
-    
-})
+menuIcon.addEventListener('click', () => {
+    menuList.classList.toggle('active');
+    menuIcon.classList.toggle('close');
+});
 
-
-function ready(){
-    if(!confirm("본 사이트는 상업적 목적이 아닌 개인 포트폴리오 용도로 제작되었습니다. 홈페이지 일부 내용과 기타 이미지 등은 출처가 따로 있음을 밝힙니다. ")){
-        window.close()
-    }else{
-        alert("신입 프론트 개발자 최문석의 포트폴리오를 보러와주셔서 감사합니다.")
+function ready() {
+    if (!confirm('본 사이트는 상업적 목적이 아닌 개인 포트폴리오 용도로 제작되었습니다. 홈페이지 일부 내용과 기타 이미지 등은 출처가 따로 있음을 밝힙니다. ')) {
+        window.close();
+    } else {
+        alert('신입 프론트 개발자 최문석의 포트폴리오를 보러와주셔서 감사합니다.');
     }
 }
 
-document.addEventListener('DOMContentLoaded',ready)
+// document.addEventListener('DOMContentLoaded', ready);
 // 포트폴리오에 추가할 멘트
 /* 
 본 사이트는 상업적 목적이 아닌 개인 포트폴리오 용도로 제작되었습니다. 홈페이지 일부 내용과 기타 이미지 등은 출처가 따로 있음을 밝힙니다.
