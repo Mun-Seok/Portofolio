a4bfc2c54f9f601bc0a370a6d65fd08bbc52840e
Mun-Seok <anstjr97@gmail.com>
2022-10-05 22:17:32 +0900

    js subPage

--- js/main.js
+++ js/main.js
@@ -1,15 +1,40 @@
 /* 변수 선언 및 초기화 */
+let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+let loadingSpinner = document.getElementById('loadingSpinner');
 let menuIcon = document.getElementById('menuIcon');
 let menuList = document.getElementById('menuList');
-let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-/* 이벤트 추가 */
+/* 함수 정의 */
+// 로딩화면
+const startLoading = () => {
+    loadingSpinner.style.display = 'block';
+};
+// 로딩화면 닫기
+const afterLoading = () => {
+    loadingSpinner.style.display = 'none';
+};
+
 // 모바일 및 테블릿 메뉴
 const toggleMenu = () => {
     if (isMobile) {
-        if (window) menuList.classList.toggle('active');
+        menuList.classList.toggle('active');
         menuIcon.classList.toggle('close');
     }
+};
+
+// 메뉴 이동
+const moveToMenu = (event) => {
+    let menuName = event.target.id;
+    if (menuName == 'introduce') {
+        window.scroll(0, 0);
+    } else if (menuName == 'skill') {
+        window.scroll(0, 650);
+    } else if (menuName == 'project') {
+        window.scroll(0, 1530);
+    } else if (menuName == 'contact') {
+        window.scroll(0, 2500);
+    }
+    toggleMenu();
 };
 
 // projectSlide 페이지네이션
@@ -44,6 +69,9 @@ const projectPagination = () => {
     }
 };
 
+// project의 아이콘 스크롤 내릴때 생성
+const showSkillIcon = () => {};
+
 // 포트폴리오 alert창
 const ready = () => {
     if (!confirm('본 사이트는 상업적 목적이 아닌 개인 포트폴리오 용도로 제작되었습니다. 홈페이지 일부 내용과 기타 이미지 등은 출처가 따로 있음을 밝힙니다. ')) {
@@ -58,8 +86,20 @@ document.addEventListener('DOMContentLoaded', () => {
     projectPagination();
 });
 
-/* 
-사진 -> 키보드로 바꾸어서 추가하기
-아이콘 스크롤시 나타남
-모바일 PC 구분
-*/
+window.addEventListener('scroll', () => {
+    //스크롤을 할 때마다 로그로 현재 스크롤의 위치가 찍혀나온다.
+    let value = window.scrollY;
+    console.log(value);
+    if (value > 300) {
+        document.getElementById('skillHTML').style.visibility = 'visible';
+    }
+    if (value > 400) {
+        document.getElementById('skillCss').style.visibility = 'visible';
+    }
+    if (value > 550) {
+        document.getElementById('skillJS').style.visibility = 'visible';
+    }
+    if (value > 800) {
+        document.getElementById('skillReact').style.visibility = 'visible';
+    }
+});
